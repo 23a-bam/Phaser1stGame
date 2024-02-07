72d9045c94866eedc44a9e0188ca96a5f17f28b5
23a-bam <158849396+23a-bam@users.noreply.github.com>
2024-02-07 20:02:48 +0200

    Додав завантаження файлів у гру, небо

--- game.js
+++ game.js
@@ -13,10 +13,21 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    // завантаження об'єктів у гру
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
 function create ()
 {
+    // додає небо, починаючи з точки (0, 0)
+    this.add.image(0, 0, 'sky').setOrigin(0, 0)
 }
 
 function update ()
