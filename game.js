f144c887ebbf8829c36d2463565b53a8d7ba5ece
23a-bam <158849396+23a-bam@users.noreply.github.com>
2024-02-07 20:06:30 +0200

    Додав платформи

--- game.js
+++ game.js
@@ -2,6 +2,14 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    // для this.physics
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -27,7 +35,16 @@ function preload ()
 function create ()
 {
     // додає небо, починаючи з точки (0, 0)
-    this.add.image(0, 0, 'sky').setOrigin(0, 0)
+    this.add.image(0, 0, 'sky').setOrigin(0, 0);
+
+    // платформи (на фіксованих позиціях)
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
